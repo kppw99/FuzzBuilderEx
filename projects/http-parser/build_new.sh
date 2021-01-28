0dddbbc7716f52bb51925ab198b90e38041d3456
ehddud758 <ehddud758@gmail.com>
2021-01-28 02:39:09 -0800

    add Target project build dockerfile

---
+++ projects/http-parser/build_new.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -eu
+
+if [ "$#" -ge "1" ] && [ $1 == "cov" ];
+then
+    CFLAGS_TEST="-m32"
+    CXXFLAGS_TEST="-m32"
+    CFLAGS="-m32 -fprofile-arcs -ftest-coverage"
+    CXXFLAGS="-m32 -fprofile-arcs -ftest-coverage"
+    LDFLAGS="-m32 -fprofile-arcs -ftest-coverage"
+    LDFLAGS_TEST="-m32 -fprofile-arcs -ftest-coverage"
+else
+    CFLAGS_TEST="-m32"
+    CXXFLAGS_TEST="-m32"
+    CFLAGS="-m32"
+    CXXFLAGS="-m32"
+    LDFLAGS="-m32 -static"
+    LDFLAGS_TEST="-m32"
+fi
+
+CC=/tool/afl-2.52b/afl-clang
+CXX=/tool/afl-2.52b/afl-clang++
+nproc=1
+SRC=.
+OUT=.
+
+make clean
+
+if [[ "$#" -ge "1" && ( $1 == "cov" || $1 == "seed" ) ]]
+then
+    make
+    make check || set ?=0
+else
+    AFL_USE_ASAN=1 make
+    ASAN_OPTIONS=detect_leaks=0 AFL_USE_ASAN=1 make check || set ?=0
+fi
+
+if [[ "$#" -ge "1" && ( $1 == "seed" ) ]]
+then
+    exit
+fi
+
+
